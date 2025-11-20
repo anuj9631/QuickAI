ef081e77d5362f3b12a0aad4ad82c038e18e321b
anuj9631 <anuj98426@gmail.com>
2025-11-20 17:15:29 +0530

    added ai tools cards options

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
+import { assets } from '../assets/assets'
 
 const Hero = () => {
 
@@ -16,6 +17,9 @@ const navigate=useNavigate()
      <div className='flex flex-wrap justify-center gap-4 text-sm max-sm:text-xs'>
       <button onClick={()=>navigate('/ai')} className='bg-primary text-white px-10 py-3 rounded-lg hover:scale-102 active:scale-95 transition cursor-pointer'>Start creating now</button>
        <button className='bg-white px-10 py-3 rounded-lg corder border-gray-300 hover:scale-102 active:scale-95 transition cursor-pointer'>Watch demo</button>
+     </div>
+     <div className='flex items-center gap-4 mt-8 mx-auto text-gray-800'>
+    <img src={assets.user_group} alt='' className='h-8'/> Trusted by 10k+ people
      </div>
     </div>
   )
