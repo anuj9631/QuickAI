2708f0f66a081776907d2a7dffb79282a57f7035
anuj9631 <anuj98426@gmail.com>
2025-11-25 16:13:58 +0530

    added conetnt of dashboard

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,8 +1,52 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { dummyCreationData } from '../assets/assets'
+import { Gem, Sparkle } from 'lucide-react'
+import { Protect, useClerk, useUser } from '@clerk/clerk-react'
 
 const Dashboard = () => {
+
+  const [creations, setCreations] = useState([])
+
+  const getDashboardData= async () => {
+    setCreations(dummyCreationData)
+  }
+
+  useEffect(()=>{
+  getDashboardData()
+  }, [])
+
   return (
-    <div>Dashboard</div>
+    <div className='h-full overflow-y-scroll p-6'>
+      <div className='flex justify-start gap-4 flex-wrap'>
+       {/*Total creation card */}
+        <div className='flex justify-between items-center w-72 p-4 px-6 bg-white rounded-xl border border-gray-200'>
+          <div className='text-slate-600'>
+            <p className='text-sm'>Total Creation</p>
+            <h2 className='text-xl font-semibold'>{creations.length}</h2>
+          </div>
+          <div className="w-10 h-10 rounded-lg bg-gradient-to-br from-[#3588F2] to-[#0BB0D7] text-white flex justify-center items-center">
+
+            <Sparkle className='w-5 text-white' />
+          </div>
+        </div>
+
+      {/*Active plan name*/}
+        <div className='flex justify-between items-center w-72 p-4 px-6 bg-white rounded-xl border border-gray-200'>
+          <div className='text-slate-600'>
+            <p className='text-sm'>Active Plan </p>
+            <h2 className='text-xl font-semibold'>
+              <Protect plan='Premium' fallback='free'>Premium</Protect>
+            </h2>
+          </div>
+          <div className="w-10 h-10 rounded-lg bg-gradient-to-br from-[#FF61C5] to-[#9E53EE] text-white flex justify-center items-center">
+
+            <Gem className='w-5 text-white' />
+          </div>
+        </div>
+
+
+      </div>
+    </div>
   )
 }
 
