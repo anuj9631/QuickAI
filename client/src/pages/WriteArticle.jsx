498abeeb43ecff2157305faa2d57df4f9a5fd981
anuj9631 <anuj98426@gmail.com>
2025-11-29 16:33:11 +0530

    fixed conflict markers

--- client/src/pages/WriteArticle.jsx
+++ client/src/pages/WriteArticle.jsx
@@ -1,70 +1,59 @@
 import React, { useState } from 'react'
-<<<<<<< HEAD
 import { Edit, Sparkles } from 'lucide-react'
-=======
-import { Sparkles } from 'lucide-react'
->>>>>>> 55fb6c28c3562271f00341e49950ede48d194868
 
 const WriteArticle = () => {
 
-  const articleLength =[
-    {length: 800, text: 'Short (500-800 words)'},
-    {length: 1200, text: 'Medium (800-1200 words)'},
-    {length: 1600, text: 'Long (1200+ words)'},
+  const articleLength = [
+    { length: 800, text: 'Short (500-800 words)' },
+    { length: 1200, text: 'Medium (800-1200 words)' },
+    { length: 1600, text: 'Long (1200+ words)' },
   ]
- const [selectedLength, setSelectedLength] = useState(articleLength[0])
- const [input, setInput] = useState('')
 
-<<<<<<< HEAD
- const onSubmitHandler = async (e) => {
-  e.preventDefault();
- }
+  const [selectedLength, setSelectedLength] = useState(articleLength[0])
+  const [input, setInput] = useState('')
+
+  const onSubmitHandler = async (e) => {
+    e.preventDefault()
+  }
 
   return (
     <div className='h-full overflow-y-scroll p-6 flex items-start flex-wrap gap-4 text-slate-700'>
-       {/* left col */}
-       <form onSubmit={onSubmitHandler}  className='w-full max-w-lg p-4 bg-white rounded-lg border border-gray-200'>
-=======
-  return (
-    <div className='h-full overflow-y-scroll p-6 flex items-start flex-wrap gap-4 text-slate-700'>
-       {/* left col */}
-       <form  className='w-full max-w-lg p-4 bg-white rounded-lg border border-gray-200'>
->>>>>>> 55fb6c28c3562271f00341e49950ede48d194868
-           <div className='flex items-center gap-3'>
-             <Sparkles className='w-6 text-[#4A7Aff]' />
-             <h1 className='text-xl font-semibold'>Article Configuration</h1>
-           </div>
-           <p className='mt-6 text-sm font-medium'>Article Topic</p>
-<<<<<<< HEAD
-           <input onChange={(e)=>setInput(e.target.value)} value={input} type='text' className='w-full p-2 px-3 mt-2 outline-none text-sm rounded-md border border-gray-300' placeholder='The future of the artificial intelligence is ....'  required/>
-=======
-           <input type='text' className='w-full p-2 px-3 mt-2 outline-none text-sm rounded-md border border-gray-300' placeholder='The future of the artificial intelligence is ....'  required/>
->>>>>>> 55fb6c28c3562271f00341e49950ede48d194868
-           <p className='mt-4 text-sm font-medium'>Article length</p>
+      <form onSubmit={onSubmitHandler} className='w-full max-w-lg p-4 bg-white rounded-lg border border-gray-200'>
+        <div className='flex items-center gap-3'>
+          <Sparkles className='w-6 text-[#4A7Aff]' />
+          <h1 className='text-xl font-semibold'>Article Configuration</h1>
+        </div>
 
-           <div className='mt-3 flex gap-3 flex-wrap sm:max-w-9/11'>
-            {articleLength.map((item, index)=>(
-              <span onClick={()=> setSelectedLength(item)} className= {`text-xs px-4 py-1 border rounded-full cursor-pointer ${selectedLength.text === item.text ? 'bg-blue-50 text-blue-700' : 'text-gray-500 border-gray-300' }`} key={index}>{item.text}</span>
-            ))}
-           </div>
-<<<<<<< HEAD
+        <p className='mt-6 text-sm font-medium'>Article Topic</p>
+        <input
+          onChange={(e) => setInput(e.target.value)}
+          value={input}
+          type='text'
+          className='w-full p-2 px-3 mt-2 outline-none text-sm rounded-md border border-gray-300'
+          placeholder='The future of the artificial intelligence is ....'
+          required
+        />
 
-           <br />
+        <p className='mt-4 text-sm font-medium'>Article length</p>
 
-           <button className='w-full flex justify-center items-center gap-2 bg-gradient-to-r from-[#226BFF] to-[#65ADFF] text-white px-4 py-2 mt-6 text-sm rounded-lg cursor-pointer'>
-            <Edit className='w-5' />
-            Generate article
-           </button>
-       </form>
-       {/* right col */}
-       {/* git check */}
-=======
-       </form>
-       {/* right col */}
->>>>>>> 55fb6c28c3562271f00341e49950ede48d194868
-       <div>
+        <div className='mt-3 flex gap-3 flex-wrap sm:max-w-9/11'>
+          {articleLength.map((item, index) => (
+            <span
+              key={index}
+              onClick={() => setSelectedLength(item)}
+              className={`text-xs px-4 py-1 border rounded-full cursor-pointer
+                ${selectedLength.text === item.text ? 'bg-blue-50 text-blue-700' : 'text-gray-500 border-gray-300'}`}
+            >
+              {item.text}
+            </span>
+          ))}
+        </div>
 
-       </div>
+        <button className='w-full flex justify-center items-center gap-2 bg-gradient-to-r from-[#226BFF] to-[#65ADFF] text-white px-4 py-2 mt-6 text-sm rounded-lg cursor-pointer'>
+          <Edit className='w-5' />
+          Generate article
+        </button>
+      </form>
     </div>
   )
 }
