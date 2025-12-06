b9b1739435c54b352cc9e0768c2e4a71704997f6
anuj9631 <anuj98426@gmail.com>
2025-12-06 14:05:35 +0530

    added clipdrop for image generation

--- server/controllers/aiController.js
+++ server/controllers/aiController.js
@@ -1,6 +1,7 @@
 import OpenAI from "openai";
 import sql from "../config/dg.js";
 import { clerkClient } from "@clerk/express";
+import axios from "axios";
 
 const AI = new OpenAI({
     apiKey: process.env.GEMINI_API_KEY,
@@ -92,4 +93,40 @@ res.json({success: true, content})
   }
 }
 
+export const generateImage = async (req, res) => {
+  try {
+    const {userId} = req.auth();
+    const {prompt, publish} = req.body;
+    const plan = req.plan;
 
+    if(plan !== 'premium'){
+      return res.json({success: false, message: "This feature is only available for premium subscriptions"})
+    }
+     
+const formData = new FormData()
+formData.append('prompt', prompt)
+
+const {data} = await axios.post("https://clipdrop-api.co/text-to-image/v1", formData, {
+  headers:{    'x-api-key': process.env.CLIPDROP_API_KEY,},
+  responseType: "arraybuffer"
+})
+
+const  base64Image = `data: image/png;base64,${Buffer.from(data, 'binary').toString('base64')}`;
+
+
+
+await sql` INSERT INTO creations(user_id, prompt, content, type) VALUES(${userId}, ${prompt}, ${content},'blog-article')`;
+
+if(plan !== 'premium'){
+  await clerkClient.users.updateUserMetadata(userId, {
+    privateMetadata :{
+      free_usage: free_usage + 1
+    }
+  })
+}
+res.json({success: true, content})
+  } catch (error) {
+    console.log(error.message)
+    res.json({success: false, message: error.message})
+  }
+}
