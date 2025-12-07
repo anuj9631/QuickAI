9b041a6650222a8af97e5cf4e8eede01e228aaa4
anuj9631 <anuj98426@gmail.com>
2025-12-07 16:28:14 +0530

    added all api

--- server/controllers/aiController.js
+++ server/controllers/aiController.js
@@ -3,6 +3,8 @@ import sql from "../config/dg.js";
 import { clerkClient } from "@clerk/express";
 import axios from "axios";
 import {v2 as cloudinary} from "cloudinary"
+import fs from 'fs';
+import * as pdf from 'pdf-parse';
 
 const AI = new OpenAI({
     apiKey: process.env.GEMINI_API_KEY,
@@ -185,6 +187,53 @@ await sql` INSERT INTO creations(user_id, prompt, content, type ) VALUES(${userI
 
 
 res.json({success: true, content: imageUrl})
+
+  } catch (error) {
+    console.log(error.message)
+    res.json({success: false, message: error.message})
+  }
+}
+
+
+export const resumeReview = async (req, res) => {
+  try {
+    const {userId} = req.auth();
+    const resume = req.file;
+    const plan = req.plan;
+   
+
+    if(plan !== 'premium'){
+      return res.json({success: false, message: "This feature is only available for premium subscriptions"})
+    }
+     
+if(resume.size > 5 *1024 *1024){
+  return res.json({success:false, message : "Resume file size exceeds allowed size(5MB)."})
+}
+
+const dataBuffer = fs.readFileSync('resume.pdf')
+const pdfData = await pdf(dataBuffer)
+
+const prompt = `Review the following resume and provide constructive feedback on its strengths, weakness, ans areas for improvement. Resume Content:\n\n${pdfData.text}`
+
+
+ const response = await AI.chat.completions.create({
+    model: "gemini-2.0-flash",
+    messages: [
+        {
+            role: "user",
+            content: prompt,
+        },
+    ],
+    temperature: 0.7, 
+    max_tokens: 1000,
+});
+    
+const content= response.choices[0].message.content
+
+await sql` INSERT INTO creations(user_id, prompt, content, type ) VALUES(${userId}, 'Review the uploaded resume', ${content},'resume-review')`;
+
+
+res.json({success: true, content})
 
   } catch (error) {
     console.log(error.message)
